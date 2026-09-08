0c8994146719586bb8cbe69bebd5a07acbb32b09
Harsh Pandey <harshpandey87091@gmail.com>
2026-09-08 21:20:33 +0530

    Time: 0 ms (100%), Space: 43.1 MB (97.39%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.java
@@ -0,0 +1,28 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        // Dutch National flag Algorithm
+        int left = 0;
+        int mid = 0;
+        int high = nums.length - 1;
+        while (mid <= high) {
+            if (nums[mid] == 0) {
+                swap(nums, left, mid);
+                left++;
+                mid++;
+            } else if (nums[mid] == 1) {
+                mid++;
+            } else {
+                // 2 wala case
+                swap(nums, mid, high);
+                high--;
+            }
+        }
+    }
+
+    public static void swap(int nums[], int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+
+    }
+}
